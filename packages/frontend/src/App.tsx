a853fdfe3f7d4de5acda5d3fa021ba7c042266db
indianaPoly <hyunrim03@naver.com>
2024-12-17 17:18:07 +0900

    fix: 경로 수정

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout/Layout";
-import MainPage from "./pages/MainPage";
+import MainPage from "./pages/main/MainPage";
 
 const App: React.FC = () => {
   return (
