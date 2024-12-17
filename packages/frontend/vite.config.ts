45073bae90a0f1345fcac1d6c77c6bc524dd2dff
indianaPoly <hyunrim03@naver.com>
2024-12-17 15:07:00 +0900

    fix: vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   server: {
     host: "0.0.0.0",
     port: 3000,
     watch: {
       usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
