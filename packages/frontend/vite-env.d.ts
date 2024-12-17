6342f2dfa677045a8df8980e598fa150953de9d1
indianaPoly <hyunrim03@naver.com>
2024-12-17 15:06:45 +0900

    fix: typescript setting

--- packages/frontend/vite-env.d.ts
+++ packages/frontend/vite-env.d.ts
@@ -1,5 +1,9 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
 interface ImportMetaEnv {
   readonly VITE_PROJECT_ID: string; // 사용하는 환경 변수 정의
+  readonly VITE_INFURA_API_KEY: string;
   // 다른 환경 변수가 있으면 여기에 추가
 }
 
